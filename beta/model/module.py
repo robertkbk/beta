0226bf65025837a5dd72ff84ddc266f32bd0b99d
robertkbk <33332309+robertkbk@users.noreply.github.com>
2024-04-13 18:55:44 +0200

    Moved to torchmetrics

--- beta/model/module.py
+++ beta/model/module.py
@@ -9,7 +9,11 @@ class BetaPredictor(pl.LightningModule):
     def __init__(self, model: BetaModel, lr: float) -> None:
         super().__init__()
         self._lr = lr
+
         self.model = model
+        self.mse = torchmetrics.MeanSquaredError()
+        self.mae = torchmetrics.MeanAbsoluteError()
+        self.md = torchmetrics.MinkowskiDistance(p=2)
 
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.model.parameters(), lr=self._lr)
@@ -24,26 +28,39 @@ class BetaPredictor(pl.LightningModule):
     def training_step(self, batch: list[Tensor]) -> Tensor:
         x, y = batch
         pred = self.model.forward(x)
-        loss = nn.functional.l1_loss(pred, y)
 
+        loss = nn.functional.l1_loss(pred, y)
         self.log("train/loss", loss)
-        self.log(
-            "train/epoch/loss", loss, on_epoch=True, on_step=False, reduce_fx="sum"
-        )
+
+        self.mse.forward(pred, y)
+        self.mae.forward(pred, y)
+        self.md.forward(pred, y)
+
         return loss
+
+    def on_train_epoch_end(self) -> None:
+        self.log("train/mse", self.mse)
+        self.log("train/mae", self.mae)
+        self.log("train/md", self.md)
 
     def validation_step(self, batch: list[Tensor]) -> Tensor:
         x, y = batch
         pred = self.model.forward(x)
 
         loss = nn.functional.mse_loss(pred, y)
-        rmsle = torchmetrics.functional.mean_squared_log_error(pred, y)
-
         self.log("val/loss", loss)
-        self.log("val/rmsle", rmsle)
+
+        self.mse.forward(pred, y)
+        self.mae.forward(pred, y)
+        self.md.forward(pred, y)
+
         return loss
+
+    def on_validation_epoch_end(self) -> None:
+        self.log("val/mse", self.mse)
+        self.log("val/mae", self.mae)
+        self.log("val/lce", self.md)
 
     def predict_step(self, batch: list[Tensor]) -> Tensor:
         pred = self.model.forward(batch[0])
-        breakpoint()
         return pred[:, -1]
