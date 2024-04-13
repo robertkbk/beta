48880ffc94686f89b6a85d5336248ee65fe7bbf5
robertkbk <33332309+robertkbk@users.noreply.github.com>
2024-04-13 18:53:32 +0200

    Added InterWeekly rate calculation scheme

--- beta/data/rates/__init__.py
+++ beta/data/rates/__init__.py
@@ -2,6 +2,7 @@ from .calculator import (
     RatesCalculator as RatesCalculator,
     Daily as Daily,
     Weekly as Weekly,
+    InterWeekly as InterWeekly,
     Monthly as Monthly,
     ReturnRate as ReturnRate,
 )
